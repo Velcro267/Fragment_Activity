77fdb3051d24496cf80060248a547aa7151a4ec2
Velcro267 <165579641+Velcro267@users.noreply.github.com>
2025-10-07 12:02:05 -0400

    Initial commit

---
+++ app/src/main/java/edu/temple/fragmentactivity/ColorFragment.kt
@@ -0,0 +1,30 @@
+package edu.temple.fragmentactivity
+
+import android.graphics.Color
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+
+class ColorFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_color, container, false).apply {
+            setBackgroundColor(
+                Color.parseColor(
+                    arrayOf("Blue", "Maroon", "Magenta", "Green", "Red").random()
+                )
+            )
+        }
+    }
+
+
+}
